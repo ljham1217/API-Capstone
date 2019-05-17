9fe42ac349425e55fb7def3718a73da1b782d054
Leah Hamilton <leahrhamilton@yahoo.com>
2019-05-17 14:40:26 -0400

    new js functions

--- script.js
+++ script.js
@@ -1,16 +1,81 @@
 'use strict'
+//watch for zipCode submission
 
-//get Market Names and ID's
-function formatQueryParameters(params) {
-    const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-
-    return queryItems.join('&');
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const zipCode= $('#js-zipCode').val();
+        console.log(zipCode);
+            getResults(zipCode);
+        //.then(date => console.log(data))
+    })
 }
 
-function displayResults(responseJson) {
-    console.log(responseJson);
+function getResults(zipCode) {
+    fetch('http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=' + zipCode)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error (response.statusText);
+    })
+    .then(responseJson => makeIdArray(responseJson))
+    .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    })
+}
 
+function makeIdArray(responseJson) {
+    console.log(responseJson);
+    const idArray= [];    
+    $.each(responseJson.results, function (i, val) {
+        idArray.push(responseJson.results[i].id);
+    });
+        console.log(idArray);
+        getDetails(idArray);       
+}
+
+/*function iterateResults(responseJson) {  
+    let marketIds = [responseJson];
+    let requests = marketIds.map(id => fetch)
+    const marketDetailsArray = [];
+        $.each(marketdetails, function(i, val){
+            marketDetailsArray.push(val.marketdetails);
+        });    
+};*/
+
+function getDetails(idArray) {
+    for (let i=0; i < idArray.length; i++)
+    fetch('http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=' + idArray[i] )
+    .then(response => {
+    if (response.ok) {
+        return response.json();
+    }
+    throw new Error (response.statusText);
+    })
+    .then (responseJson => marketDetailsArray(responseJson))
+
+    .catch(err => {
+    $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function marketDetailsArray(responseJson, idArray) {
+    console.log(responseJson);
+    const market = [responseJson];
+    console.log(market);
+    $.forEach(responseJson).concat(responseJson);
+    const marketDetail = {
+        market: [responseJson],
+        name: [],
+        address: [],
+        googlelink: [],
+        products: [],
+        schedule: [],
+    }
+}
+
+/*function displayResults(results) {
     $('.searchResults').empty();
   
     for(let i=0; i < responseJson.results.length; i++){
@@ -19,56 +84,30 @@ function displayResults(responseJson) {
         <h1>${responseJson.results[i].marketname}</h1></div`
       )};
       $('#js-results').removeClass('hidden');
-      let marketID = responseJson.results[i].id;
-      getDetails(responseJson);
-  }
-  
-  function getDetails(marketID) {
-    $.ajax({
-        type: "GET",
-        contentType: "application/json; charset=utf-8",
-        // submit a get request to the restful service mktDetail.
-        url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + marketId,
-        dataType: 'jsonp',
-        jsonpCallback: 'detailResultHandler'
-    });
-}
-function getResults(zipCode) {
-    console.log(zipCode);
-    const searchURL = 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch';
-
-    const params = {
-        zip: zipCode,
-    }
-
-    console.log(params);
-
-    const queryString = formatQueryParameters(params);
-    const url =  searchURL + '?' + queryString;
-
-    console.log(url);
-
-    fetch(url)
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-        throw new Error (response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-        $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    })
+      getDetails(marketIdArray);
 }
 
-function watchForm() {
-    $('form').submit(event => {
-        event.preventDefault();
-        const zipCode= $('#js-zipCode').val();
-        console.log(zipCode);
-        getResults(zipCode)
+
+/*$('.searchResults').empty();
+    
+    for (let i=0; i < detailresults.length; i++)
+    
+    $('.searchResults').append(
+        `<div class="cards>
+        <h1>${detailresults}</h1>
+        </div>`
+    );
+    $('#js-results').removeClass('hidden');
+)   detailresults, function (i, val) {
+        detailresults.push(val.marketdetails);
+        console.log(marketDetail);
     })
-}
+
+for (var key in detailresults) {
+    console.log(key);
+    var results = detailresults[key];
+    displayResults(results);
+}*/
 
 $(function(){
     console.log('App loaded! Waiting for submit!');
